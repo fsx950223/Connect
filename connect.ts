ce35f611fe48f73785de36f41723a4a14f00e99c
who who who <fsx950223@outlook.com>
2017-11-18 23:02:47 +0800

    Update connect.ts

--- connect.ts
+++ connect.ts
@@ -4,7 +4,7 @@
  * @interface IRequestParam
  */
 interface IRequestParam {
-    readonly [propName: string]: any;
+    readonly[propName : string] : any;
 }
 /**
  * 头参数接口
@@ -12,13 +12,13 @@ interface IRequestParam {
  * @interface IHeaderParam
  */
 interface IHeaderParam {
-    readonly [propName: string]: string;
+    readonly[propName : string] : string;
 }
 
 export const enum CredentialsEnums {
     omit,
     sameOrigin,
-    include,
+    include
 }
 export const enum CacheEnums {
     default,
@@ -26,24 +26,24 @@ export const enum CacheEnums {
     reload,
     noCache,
     forceCache,
-    onlyIfCached,
+    onlyIfCached
 }
 
 export const enum RedirectEnums {
     follow,
     error,
-    manual,
+    manual
 }
 
 export const enum ModeEnums {
     cors,
     noCors,
-    sameOrigin,
+    sameOrigin
 }
 
 export const enum ReferrerEnums {
     noReferrer,
-    client,
+    client
 }
 
 export const enum ReferrerPolicyEnums {
@@ -51,7 +51,7 @@ export const enum ReferrerPolicyEnums {
     noReferrerWhenDowngrade,
     origin,
     originWhenCrossOrigin,
-    unsafeUrl,
+    unsafeUrl
 }
 /**
  * 选项接口
@@ -72,51 +72,28 @@ interface IOptions {
 }
 
 export interface IConnect {
-    post(url: string, requestParam: any, headerParam?: IHeaderParam): Promise<any>;
-    get(url: string, requestParam?: IRequestParam, headerParam?: IHeaderParam): Promise<any>;
-    put(url: string, requestParam?: any, headerParam?: IHeaderParam): Promise<any>;
-    delete(url: string, requestParam?: IRequestParam, headerParam?: IHeaderParam): Promise<any>;
-    patch(url: string, requestParam?: any, headerParam?: IHeaderParam): Promise<any>;
-    head(url: string, requestParam?: IRequestParam, headerParam?: IHeaderParam): Promise<any>;
-    options(url: string, requestParam?: IRequestParam, headerParam?: IHeaderParam): Promise<any>;
+    post(url : string, requestParam : any, headerParam?: IHeaderParam) : Promise < any >;
+    get(url : string, requestParam?: IRequestParam, headerParam?: IHeaderParam) : Promise < any >;
+    put(url : string, requestParam?: any, headerParam?: IHeaderParam) : Promise < any >;
+    delete(url : string, requestParam?: IRequestParam, headerParam?: IHeaderParam) : Promise < any >;
+    patch(url : string, requestParam?: any, headerParam?: IHeaderParam) : Promise < any >;
+    head(url : string, requestParam?: IRequestParam, headerParam?: IHeaderParam) : Promise < any >;
+    options(url : string, requestParam?: IRequestParam, headerParam?: IHeaderParam) : Promise < any >;
 }
 
 export class Connect implements IConnect {
-    private headerParam: IHeaderParam;
-    private origin: string;
-    private messageKey: string;
-    private credentials: RequestCredentials;
-    private cache: RequestCache;
-    private redirect: RequestRedirect;
-    private referrer: string;
-    private referrerPolicy: ReferrerPolicy;
-    private mode: RequestMode;
-    private integrity: string;
-    constructor(private domain: string, options?: IOptions) {
-        if (options) {
-            const {
-                origin,
-                headerParam,
-                messageKey,
-                credentials,
-                cache,
-                redirect,
-                referrer,
-                referrerPolicy,
-                mode,
-                integrity,
-            } = options;
-            if (origin) { this.origin = origin; }
-            if (headerParam) { this.headerParam = { ...this.headerParam, ...headerParam }; }
-            if (messageKey) { this.messageKey = messageKey; }
-            if (credentials) { this.credentials = credentials; }
-            if (cache) { this.cache = cache; }
-            if (redirect) { this.redirect = redirect; }
-            if (referrer) { this.referrer = referrer; }
-            if (referrerPolicy) { this.referrerPolicy = referrerPolicy; }
-            if (mode) { this.mode = mode; }
-            if (integrity) { this.integrity = integrity; }
-        }
+    private headerParam : IHeaderParam;
+    private origin : string;
+    private messageKey : string;
+    private credentials : RequestCredentials;
+    private cache : RequestCache;
+    private redirect : RequestRedirect;
+    private referrer : string;
+    private referrerPolicy : ReferrerPolicy;
+    private mode : RequestMode;
+    private integrity : string;
+    constructor(private domain : string, options?: IOptions) {
+        this.setKeys(this, options);
     }
     /**
      * 初始化全局参数
@@ -126,73 +103,66 @@ export class Connect implements IConnect {
      * @param {IHeaderParam} headerParam 全局头参数
      * @memberof Connect
      */
-    static init(options: IOptions) {
-        const {
-            origin,
-            headerParam,
-            messageKey,
-            credentials,
-            cache,
-            redirect,
-            referrer,
-            referrerPolicy,
-            mode,
-            integrity,
-        } = options;
-        Connect.prototype.origin = origin;
-        Connect.prototype.headerParam = headerParam;
-        Connect.prototype.messageKey = messageKey;
-        Connect.prototype.credentials = credentials;
-        Connect.prototype.cache = cache;
-        Connect.prototype.redirect = redirect;
-        Connect.prototype.referrer = referrer;
-        Connect.prototype.referrerPolicy = referrerPolicy;
-        Connect.prototype.mode = mode;
-        Connect.prototype.integrity = integrity;
+    static init(options : IOptions) {
+        Connect
+            .prototype
+            .setKeys(Connect.prototype, options);
     }
 
-    async post(url: string, requestParam: IRequestParam, options?: IOptions): Promise<any> {
+    async post(url : string, requestParam : IRequestParam, options?: IOptions) : Promise < any > {
         url = this.resolveUrl(this.origin, this.domain, url);
-        const headers = new Headers({ ...this.headerParam, ...options.headerParam });
+        const headers = new Headers({
+            ...this.headerParam,
+            ...options.headerParam
+        });
         const requestBody = this.detectRequestParam(requestParam, headers.get('Content-Type'));
         const request = new Request(url, {
             method: 'post',
             body: requestBody,
             headers,
-            ...this.generateOptions(options),
+            ...this.generateOptions(options)
         });
         return this.pretreatment(await fetch(request));
     }
-    async get(url: string, requestParam?: IRequestParam, options?: IOptions): Promise<any> {
-        const headers = new Headers({ ...this.headerParam, ...options.headerParam });
+    async get(url : string, requestParam?: IRequestParam, options?: IOptions) : Promise < any > {
+        const headers = new Headers({
+            ...this.headerParam,
+            ...options.headerParam
+        });
         url = this.setUrl(url, requestParam);
         const request = new Request(url, {
             method: 'get',
             headers,
-            ...this.generateOptions(options),
+            ...this.generateOptions(options)
         });
         return this.pretreatment(await fetch(request));
     }
-    async put(url: string, requestParam: IRequestParam, options?: IOptions): Promise<any> {
+    async put(url : string, requestParam : IRequestParam, options?: IOptions) : Promise < any > {
         url = this.resolveUrl(this.origin, this.domain, url);
-        const headers = new Headers({ ...this.headerParam, ...options.headerParam });
+        const headers = new Headers({
+            ...this.headerParam,
+            ...options.headerParam
+        });
         const requestBody = this.detectRequestParam(requestParam, headers.get('Content-Type'));
         const request = new Request(url, {
             method: 'put',
             body: requestBody,
             headers,
-            ...this.generateOptions(options),
+            ...this.generateOptions(options)
         });
         return this.pretreatment(await fetch(request));
     }
 
-    async delete(url: string, requestParam?: IRequestParam, options?: IOptions): Promise<any> {
-        const headers = new Headers({ ...this.headerParam, ...options.headerParam });
+    async delete(url : string, requestParam?: IRequestParam, options?: IOptions) : Promise < any > {
+        const headers = new Headers({
+            ...this.headerParam,
+            ...options.headerParam
+        });
         url = this.setUrl(url, requestParam);
         const request = new Request(url, {
             method: 'delete',
             headers,
-            ...this.generateOptions(options),
+            ...this.generateOptions(options)
         });
         return this.pretreatment(await fetch(request));
     }
@@ -205,15 +175,18 @@ export class Connect implements IConnect {
      * @returns {Promise<any>} 返回预处理后的Promise
      * @memberof Connect
      */
-    async patch(url: string, requestParam: IRequestParam, options?: IOptions): Promise<any> {
+    async patch(url : string, requestParam : IRequestParam, options?: IOptions) : Promise < any > {
         url = this.resolveUrl(this.origin, this.domain, url);
-        const headers = new Headers({ ...this.headerParam, ...options.headerParam });
+        const headers = new Headers({
+            ...this.headerParam,
+            ...options.headerParam
+        });
         const requestBody = this.detectRequestParam(requestParam, headers.get('Content-Type'));
         const request = new Request(url, {
             method: 'patch',
             body: requestBody,
             headers,
-            ...this.generateOptions(options),
+            ...this.generateOptions(options)
         });
         return this.pretreatment(await fetch(request));
     }
@@ -226,13 +199,16 @@ export class Connect implements IConnect {
      * @returns {Promise<any>} 返回预处理后的Promise
      * @memberof Connect
      */
-    async head(url: string, requestParam?: IRequestParam, options?: IOptions): Promise<any> {
-        const headers = new Headers({ ...this.headerParam, ...options.headerParam });
+    async head(url : string, requestParam?: IRequestParam, options?: IOptions) : Promise < any > {
+        const headers = new Headers({
+            ...this.headerParam,
+            ...options.headerParam
+        });
         url = this.setUrl(url, requestParam);
         const request = new Request(url, {
             method: 'head',
             headers,
-            ...this.generateOptions(options),
+            ...this.generateOptions(options)
         });
         return this.pretreatment(await fetch(request));
     }
@@ -246,13 +222,16 @@ export class Connect implements IConnect {
      * @returns {Promise<any>} 返回预处理后的Promise
      * @memberof Connect
      */
-    async options(url: string, requestParam?: IRequestParam, options?: IOptions): Promise<any> {
-        const headers = new Headers({ ...this.headerParam, ...options.headerParam });
+    async options(url : string, requestParam?: IRequestParam, options?: IOptions) : Promise < any > {
+        const headers = new Headers({
+            ...this.headerParam,
+            ...options.headerParam
+        });
         url = this.setUrl(url, requestParam);
         const request = new Request(url, {
             method: 'options',
             headers,
-            ...this.generateOptions(options),
+            ...this.generateOptions(options)
         });
         return this.pretreatment(await fetch(request));
     }
@@ -264,7 +243,7 @@ export class Connect implements IConnect {
      * @returns {Promise<any>} 预处理后的Promise
      * @memberof Connect
      */
-    private async pretreatment(response: Response): Promise<any> {
+    private async pretreatment(response : Response) : Promise < any > {
         try {
             const result = await response.json();
             if (response.ok) {
@@ -284,11 +263,13 @@ export class Connect implements IConnect {
      * @returns 拼接完成的url
      * @memberof Connect
      */
-    private setUrl(url: string, requestParam?: IRequestParam) {
+    private setUrl(url : string, requestParam?: IRequestParam) {
         const urlObject = new URL(this.resolveUrl(this.origin, this.domain, url));
         for (const param in requestParam) {
             if (requestParam.hasOwnProperty(param)) {
-                urlObject.searchParams.set(param, requestParam[param]);
+                urlObject
+                    .searchParams
+                    .set(param, requestParam[param]);
             }
         }
         return urlObject.toString();
@@ -305,49 +286,8 @@ export class Connect implements IConnect {
         return params.join('/');
     }
 
-    private generateRequest(options: any) {
-        const {
-            origin,
-            headers,
-            credentials,
-            cache,
-            redirect,
-            referrer,
-            referrerPolicy,
-            mode,
-            integrity,
-            url,
-            requestParam,
-        } = options;
-        if (requestParam) {
-            return new Request(url, {
-                method: 'patch',
-                body: requestParam,
-                headers,
-                credentials,
-                cache,
-                redirect,
-                referrer,
-                referrerPolicy,
-                mode,
-                integrity,
-            });
-        } else {
-            return new Request(url, {
-                method: 'options',
-                headers,
-                credentials,
-                cache,
-                redirect,
-                referrer,
-                referrerPolicy,
-                mode,
-                integrity,
-            });
-        }
-    }
-    private detectRequestParam(requestParam: IRequestParam, contentType: string) {
-        const generateFormData = (request: IRequestParam) => {
+    private detectRequestParam(requestParam : IRequestParam, contentType : string) {
+        const generateFormData = (request : IRequestParam) => {
             const formData = new FormData();
             for (const param in request) {
                 if (request.hasOwnProperty(param)) {
@@ -357,8 +297,10 @@ export class Connect implements IConnect {
             return formData;
         };
         switch (contentType) {
-            case 'application/json': return JSON.stringify(requestParam);
-            case 'multipart/form-data': return generateFormData(requestParam);
+            case 'application/json':
+                return JSON.stringify(requestParam);
+            case 'multipart/form-data':
+                return generateFormData(requestParam);
         }
     }
 
@@ -369,7 +311,19 @@ export class Connect implements IConnect {
             cache: options.cache || this.cache,
             referrer: options.referrer || this.referrer,
             referrerPolicy: options.referrerPolicy || this.referrerPolicy,
-            integrity: options.integrity || this.integrity,
+            integrity: options.integrity || this.integrity
         };
+    }
+
+    private setKeys(source, options) {
+        if (options) {
+            const keys = Reflect
+                .ownKeys(source)
+                .filter(key => typeof key !== 'function' && key !== 'constructor');
+            for (const key in keys) {
+                if (key in options) 
+                    source[key] = options[key];
+                }
+            }
     }
 }
